b36c2ad33f1f707dc601cf0725cae1d935a0eb34
kleber <muruchi2018@gmail.com>
2024-08-16 12:16:58 -0400

    2do commit de main

--- hola.java
+++ hola.java
@@ -1,5 +1,9 @@
  public class hola {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde Java!");
+        int a = aleatorio();
+        System.out.println("Hola mundo desde Java! mi numero aleatorio es : "+a );
     }
-}
+    public static int aleatorio () {
+        return (int)(Math.random()*100);  // Devuelve un número aleatorio entre 0 y 99
+    }
+ }
